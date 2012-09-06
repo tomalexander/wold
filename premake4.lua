9db508cddf2545c9c8f5c9d977581fceaaa9439b
Tom Alexander <craftkiller@gmail.com>
2012-09-05 22:14:23 -0400

    experiment

--- premake4.lua
+++ premake4.lua
@@ -51,7 +51,7 @@ function link_sfml()
    configuration "Debug or Release"
    links {"sfml-window", "sfml-graphics", "sfml-audio", "sfml-network", "sfml-system"}
    configuration {"linux", "Static*"}
-   linkoptions {"/usr/local/lib/libsfml-window-s.a", "/usr/local/lib/libsfml-graphics-s.a", "/usr/local/lib/libl-audio-s.a", "/usr/local/lib/libl-network-s.a", "/usr/local/lib/libl-system-s.a"}
+   linkoptions {"/usr/local/lib/libsfml-window-s.a", "/usr/local/lib/libsfml-graphics-s.a", "/usr/local/lib/lib-audio-s.a", "/usr/local/lib/lib-network-s.a", "/usr/local/lib/lib-system-s.a"}
    configuration {"windows", "Static*"}
    links {"sfml-window-s", "sfml-graphics-s", "sfml-audio-s", "sfml-network-s", "sfml-system-s"}
    configuration {}
